f0d46205d5d8c40ad8752ab9ce27e57f91f46395
jaime barbudo <jaimebarbudomier@gmail.com>
2025-02-25 00:14:04 -0500

    Remove Header, Hero, and Footer components from Home page and add placeholder text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import Header from "../components/header";
-import Footer from "../components/footer";
-import Hero from "../components/hero";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
-      <Hero />
-      <Footer />
+      
+     <p>hola mundo</p>
     </div>
   );
 }
