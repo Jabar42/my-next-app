8cf6fffad3bdf7a0ea74570a1a717739a0835b47
jaime barbudo <jaimebarbudomier@gmail.com>
2025-02-25 01:00:22 -0500

    Add Hero and Footer components to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import Hero from "@/components/hero";
 import Prueba from "../components/prueba";
+import Footer from "@/components/footer";
 
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Prueba/>
+      <Hero/>
+      <Footer/>
     </div>
   );
 }
